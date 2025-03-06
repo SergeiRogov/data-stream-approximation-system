534ea6e1bd730438641d70be29dc45ba080c221d
SergeiRogov <rogov120301@gmail.com>
2025-03-06 23:37:02 +0200

    add method implementation

--- summarization_algorithms/count_min_sketch.py
+++ summarization_algorithms/count_min_sketch.py
@@ -10,7 +10,7 @@ class CountMinSketch(CountMinSketchBase):
         Initialize sketch with width, depth, and seed.
         """
         super().__init__(width, depth)
-        self.tables = [array.array("l", (0 for _ in range(self.width))) for _ in range(self.depth)]
+        self.hash_tables = [array.array("l", (0 for _ in range(self.width))) for _ in range(self.depth)]
 
     def _hash(self, x):
         """
@@ -22,11 +22,15 @@ class CountMinSketch(CountMinSketchBase):
             yield int(base_hash.hexdigest(), 16) % self.width
 
     def add(self, item, count=1):
-        pass
+        """
+        Add the element 'item' as if it had appeared 'count' times
+        """
+        self.totalCount += count
+        for table, i in zip(self.hash_tables, self._hash(item)):
+            table[i] += count
 
     def query(self, item):
         pass
 
     def reset(self):
         pass
-
