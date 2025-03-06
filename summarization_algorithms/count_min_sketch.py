91e683be65541d5cc33b3e68e3b430aa1c8c4bcb
SergeiRogov <rogov120301@gmail.com>
2025-03-06 23:26:17 +0200

    Private hashing method

--- summarization_algorithms/count_min_sketch.py
+++ summarization_algorithms/count_min_sketch.py
@@ -1,5 +1,6 @@
 from count_min_sketch_base import CountMinSketchBase
 import array
+import hashlib
 
 
 class CountMinSketch(CountMinSketchBase):
@@ -10,6 +11,15 @@ class CountMinSketch(CountMinSketchBase):
         """
         super().__init__(width, depth)
         self.tables = [array.array("l", (0 for _ in range(self.width))) for _ in range(self.depth)]
+
+    def _hash(self, x):
+        """
+        Generate multiple hash values for a given input item `x`
+        """
+        base_hash = hashlib.md5(str(hash(x)).encode('utf-8'))
+        for i in range(self.depth):
+            base_hash.update(str(i).encode('utf-8'))
+            yield int(base_hash.hexdigest(), 16) % self.width
 
     def add(self, item, count=1):
         pass
