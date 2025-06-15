722dd25db1d73774c5cad4f635121cebdb9fbfd2
SergeiRogov <rogov120301@gmail.com>
2025-06-15 19:52:34 +0300

    Query Time measurement modification

--- evaluation/avg_query_time.py
+++ evaluation/avg_query_time.py
@@ -2,26 +2,33 @@ import random
 import time
 
 
-def evaluate_avg_query_time(cms, ground_truth, test_samples_num=1000):
+def evaluate_avg_query_time(cms, ground_truth, threshold=100000):
     """
     Evaluates the average time of the query method of Sketch variation.
 
     Args:
         cms: The CountMinSketch instance to test.
         ground_truth: A dictionary containing the actual counts of items.
-        test_samples_num: The number of random items to query.
+        threshold: The size above which sampling is used.
 
     Returns:
         Average query time per item.
     """
-    test_items = random.sample(list(ground_truth.keys()), min(test_samples_num, len(ground_truth)))
+    total_items = len(ground_truth)
+    if not total_items:  # nothing to evaluate
+        return 0
+
+    if total_items > threshold:
+        test_items = random.sample(list(ground_truth.keys()), threshold)  # randomly sample 'threshold' items
+    else:
+        test_items = list(ground_truth.keys())
 
     start_time = time.time()
     for item in test_items:
         cms.query(item)
     end_time = time.time()
 
-    avg_query_time = (end_time - start_time) / test_samples_num
+    avg_query_time = (end_time - start_time) / len(test_items)
 
     return avg_query_time
 
